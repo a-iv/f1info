142377fca0426df39c930efc64981803963b54c3
alexander.ivanov <alexander.ivanov@192.168.58.57>
2010-03-12 19:11:07 +0500

    Добавил автозаполнение строки результата

--- media/js/admin.js
+++ media/js/admin.js
@@ -1,3 +1,13 @@
 $().ready(function() {
-	$("select").not("td.team select,td.engine select,td.tyre select").sexyCombo();
+//	$("select").not("td.team select,td.engine select,td.tyre select").sexyCombo();
+	$("td.racer select").change(function() {
+		var select = this;
+		$.getJSON("/racer_last_info/" + select.value + "/", function(data) {
+			var row = $(select).parent().parent();
+			$("td.team select", row)[0].value = data.team;
+			$("td.tyre select", row)[0].value = data.tyre;
+			$("td.engine select", row)[0].value = data.engine;
+		});		
+	});
+	
 });
