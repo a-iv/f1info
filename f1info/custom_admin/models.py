d15fa3e26adcf00df0ac849f7fac3679dc4827fe
elky <mansellfan@gmail.com>
2010-10-30 22:26:17 +0600

    Remove all migrations and unused templates

--- f1info/custom_admin/models.py
+++
@@ -1,5 +0,0 @@
-import re
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
-UserCreationForm.base_fields['username'].regex = re.compile(r'^[-\.\w]+$')
-UserChangeForm.base_fields['username'].regex = re.compile(r'^[-\.\w]+$')
