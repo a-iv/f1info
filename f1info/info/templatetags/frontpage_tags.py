e4ab13860dbc988096c8cd6a6faa22016875243e
elky.netbook <mansellfan@gmail.com>
2010-12-26 00:52:09 +0500

    Fix template tag for display next grand prix on frontpage

--- f1info/info/templatetags/frontpage_tags.py
+++ f1info/info/templatetags/frontpage_tags.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django import template
-from f1info.models import Heat
+from f1info.models import GrandPrix, Heat
 import datetime
 
 register = template.Library()
 
 @register.inclusion_tag('f1info/tags/nextgp.html', takes_context=False)
 def show_nextgp():
-    next = Heat.objects.all()
     current = datetime.datetime.today()
-    return {'nextgp': next, 'current': current,}
+    try:
+        next = Heat.objects.filter(date__gte=current)[:1]
+        gp = GrandPrix.objects.filter(heats=next)
+        start = Heat.objects.filter(grandprix=gp)[0]
+        end = Heat.objects.filter(grandprix=gp).latest('date')
+        return {'nextgp': next, 'current': current, 'start': start, 'end': end,}
+    except:
+        pass
