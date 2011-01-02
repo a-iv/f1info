ecfc6e26fd9dd51efc58a8f21d59416a6a9afbd8
elky <mansellfan@gmail.com>
2011-01-02 20:26:04 +0000

    Update django to 1.2 and fix admins email

--- f1info/settings.py
+++ f1info/settings.py
@@ -5,12 +5,12 @@ import os
 gettext_noop = lambda s: s
 
 ADMINS = (
-    ('webmaster', 'webmaster@redsolution.ru'),
+    ('f1online', 'mansellfan@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
-EMAIL_SUBJECT_PREFIX = '[f1info]'
+EMAIL_SUBJECT_PREFIX = '[f1online]'
 
 DATABASE_ENGINE = 'postgresql_psycopg2'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'f1info'
